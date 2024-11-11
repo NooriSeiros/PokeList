7bde6b8dee65333a9f52ada17b44bca8798c18a5
Noori Seiros <nooriseiros@gmail.com>
2024-11-11 13:14:03 -0300

    Inicio do Projeto

---
+++ src/main/java/com/noori/pokelist/PokelistApplication.java
@@ -0,0 +1,13 @@
+package com.noori.pokelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noori/pokelist/PokelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noori.pokelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
