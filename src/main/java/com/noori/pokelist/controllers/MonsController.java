2475c5039208a43f0e6fbc2e464149d7aff12e1d
Noori Seiros <nooriseiros@gmail.com>
2024-11-13 13:27:53 -0300

    GET game by id

--- src/main/java/com/noori/pokelist/controllers/MonsController.java
+++ src/main/java/com/noori/pokelist/controllers/MonsController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.noori.pokelist.dto.MonsDTO;
 import com.noori.pokelist.dto.MonsMinDTO;
-import com.noori.pokelist.entities.Mons;
 import com.noori.pokelist.services.MonsService;
 
 @RestController
@@ -17,6 +18,12 @@ public class MonsController {
 	
 	@Autowired
 	private MonsService monsService;
+	
+	@GetMapping(value = "/{id}")
+	public MonsDTO findById(@PathVariable Long id){
+		MonsDTO result = monsService.findById(id);
+		return result;
+	}
 	
 	@GetMapping
 	public List<MonsMinDTO> findAll(){
--- src/main/java/com/noori/pokelist/services/MonsService.java
+++ src/main/java/com/noori/pokelist/services/MonsService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.noori.pokelist.dto.MonsDTO;
 import com.noori.pokelist.dto.MonsMinDTO;
 import com.noori.pokelist.entities.Mons;
 import com.noori.pokelist.repositories.MonsRepository;
@@ -13,11 +15,21 @@ import com.noori.pokelist.repositories.MonsRepository;
 public class MonsService {
 	
 	@Autowired
-	MonsRepository gameRepository;
+	MonsRepository monsRepository;
 	
+	@Transactional(readOnly = true)
+	public MonsDTO findById(Long id) {
+		Mons result = monsRepository.findById(id).get();
+		MonsDTO dto = new MonsDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<MonsMinDTO> findAll() {
-		List<Mons> result = gameRepository.findAll();
+		List<Mons> result = monsRepository.findAll();
 		List<MonsMinDTO> dto = result.stream().map(x -> new MonsMinDTO(x)).toList();
 		return dto;
 	}
+	
+	
 }
